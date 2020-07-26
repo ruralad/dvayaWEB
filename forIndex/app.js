0f6d4c97382203534735928e794b672e82919520
eesypeesy <46086523+eesypeesy@users.noreply.github.com>
2020-07-26 16:30:41 +0530

    Pure css navbar

--- forIndex/app.js
+++ forIndex/app.js
@@ -1,14 +1,1 @@
-console.log('Getting Into Script O.K')
-
-function show(div) {
-    document.getElementById(div).style.display = 'block';
-}
-
-function hide(div) {
-    document.getElementById(div).style.display = 'none';
-}
-window.onclick = function(event) {
-    if (event.target !== document.getElementById('academicsBar')) document.getElementById('academicsDrop').style.display = "none";
-    if(event.target !== document.getElementById('communitiesBar')) document.getElementById('communitiesDrop').style.display = "none";
-    if(event.target !== document.getElementById('sfiBar'))document.getElementById('sfiDrop').style.display = "none";
-  }
+console.log('Getting Into app.js');
