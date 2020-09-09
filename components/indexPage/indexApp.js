ece904b0a87525cf29d855ae63b22caaf5a1ea4a
eesypeesy <46086523+eesypeesy@users.noreply.github.com>
2020-09-09 21:10:26 +0530

    overhaul

--- components/indexPage/indexApp.js
+++ components/indexPage/indexApp.js
@@ -1,19 +1,13 @@
 console.log('Getting Into app.js');
 
 const menuBurger = document.getElementById('menuBurger');
-const navBar = document.getElementById('navBar');
-const indexBody = document.getElementById('hero');
 let menuBurgerOpen = false;
 
 menuBurger.onclick = () => {    
     if(!menuBurgerOpen){
         menuBurger.classList.toggle('is-opened');
-        navBar.style.display = 'flex';
-        indexBody.style.display = 'none';
     }else{
         menuBurger.classList.toggle('is-opened');
-        navBar.style.display = 'none';
-        indexBody.style.display = 'flex';
     }
     menuBurgerOpen =!menuBurgerOpen;
 }
