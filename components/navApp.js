bc722d4be0691cbd4cfa88f137d5e03e75d3eeb8
eesypeesy <46086523+eesypeesy@users.noreply.github.com>
2020-09-10 21:56:29 +0530

    9sep sfiPageChanges

--- components/navApp.js
+++ components/navApp.js
@@ -4,28 +4,28 @@ let menuBurgerOpen = false;
 
 function showDropdown() {
 
-    if(!menuBurgerOpen){
+    if (!menuBurgerOpen) {
         menuBurger.classList.toggle('is-opened');
         dropDown.style.display = "block";
-    }else{
+    } else {
         menuBurger.classList.toggle('is-opened');
         dropDown.style.display = "none";
     }
-    menuBurgerOpen =!menuBurgerOpen;
+    menuBurgerOpen = !menuBurgerOpen;
 }
 
 //set favicon
 function change_favicon(img) {
     let favicon = document.querySelector('link[rel="shortcut icon"]');
-    
+
     if (!favicon) {
         favicon = document.createElement('link');
         favicon.setAttribute('rel', 'shortcut icon');
         let head = document.querySelector('head');
         head.appendChild(favicon);
     }
-    
-    
+
+
     favicon.setAttribute('type', 'image/png');
     favicon.setAttribute('href', '../assets/dvayaLogo.png');
 }
