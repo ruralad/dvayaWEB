7919c82d71b967d79271ea28212ad49b81162c1c
eesypeesy <46086523+eesypeesy@users.noreply.github.com>
2020-10-03 15:38:38 +0530

    Update helpdesk.js

--- components/helpdesk/helpdesk.js
+++ components/helpdesk/helpdesk.js
@@ -1,4 +1,4 @@
 async function calls() {
     let response = await fetch('https://dvaya-beta.netlify.app/api/freshers-notification.json');
-    console.log(response);
+    console.log(response.json());
 }
